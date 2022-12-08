4f9e1d8a84e9efb635d4e12a6f2e749e50d00a10
KekOnTheWorld <kekontheworld@gmail.com>
2022-12-08 15:35:26 +0100

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+</script>
+
+<svelte:head>
+	<title>KoCAD</title>
+</svelte:head>
+
+<main>
+    <h1>KekCAD</h1>
+    <p>The only CAD Program you will ever need!</p>
+</main>
+
+<style lang="postcss">
+    main {
+        
+    }
+</style>
+
