0c7d2adc21476b765ca065fe2e55446f88ef4bff
KekOnTheWorld <kekontheworld@gmail.com>
2022-12-08 16:50:14 +0100

    Made loading page work

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,3 @@
-<script lang="ts">
-</script>
-
 <svelte:head>
 	<title>KoCAD</title>
 </svelte:head>
@@ -8,11 +5,34 @@
 <main>
     <h1>KekCAD</h1>
     <p>The only CAD Program you will ever need!</p>
+    <div class="links">
+        <a href="new">New Sketch</a>
+        <a href="load">Load Sketch</a>
+    </div>
 </main>
 
 <style lang="postcss">
     main {
-        
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        min-height: 100vh;
+    }
+
+    .links {
+        margin-top: 20px;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        row-gap: 5px;
+    }
+
+    .links a {
+        padding: 5px 10px;
+        background-color: #111;
+        border: 2px solid rgba(255,255,255,0.1);
+        border-radius: 10px;
     }
 </style>
 
