19478f81e592f6dc9d30348fe21d2a0ade88d7e5
KekOnTheWorld <kekontheworld@gmail.com>
2022-12-10 19:14:11 +0100

    Added Component for editing sketches

---
+++ src/lib/EditSketch.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import type { Sketch } from "./cad/sketch";
+
+    export let sketch: Sketch;
+
+    let svgRect = false;
+    setInterval(() => svgRect = !svgRect, 1000);
+</script>
+
+<main>
+    <svg>
+        {#if svgRect}
+            <rect width="20" height="20" fill="purple"/>
+        {/if}
+    </svg>
+</main>
